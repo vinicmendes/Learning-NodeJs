96366c10f300691e3527aa2d22903cc473da9d2e
vinicmendes <vinicius.o.mendes@ufv.br>
2022-08-14 15:59:14 -0300

    up_1.0.2

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const { randomUUID } = require('crypto');
+const fs = require('fs');
 
 const app = express();
 
 app.use(express.json())
 
-const products = [];
+let products = [];
 
+fs.readFile('products.json',"utf-8" ,(err, data) => {
+    if(err) console.log(err);
+    else products = JSON.parse(data);
+});
 
 /**
  * POST -> INSERIR DADO
@@ -22,8 +27,8 @@ const products = [];
  */
 
 app.post("/products", (req, res) => {
-    const {name,price} = req.body;
-    
+    const { name, price } = req.body;
+
     const product = {
         name,
         price,
@@ -31,6 +36,14 @@ app.post("/products", (req, res) => {
     };
 
     products.push(product);
+
+    fs.writeFile("products.json", JSON.stringify(products), (err) => {
+        if (err) {
+            console.log(err)
+        } else {
+            console.log("Arquivo salvo com sucesso")
+        }
+    });
 
     return res.json(product);
 });
@@ -40,11 +53,39 @@ app.get("/products", (req, res) => {
 });
 
 app.get("/products/:id", (req, res) => {
-    const {id} = req.params;
+    const { id } = req.params;
 
     const product = products.find(product => product.id === id);
 
     return res.json(product);
+});
+
+app.put("/products/:id", (req, res) => {
+    const { id } = req.params;
+    const { name, price } = req.body;
+
+    const productIndex = products.findIndex(product => product.id === id);
+    products[productIndex] = {
+        /**
+         * Rest Operator -> pega todos, exceto name e price
+         */
+        ...products[productIndex],
+        name,
+        price
+    };
+
+
+    return res.json({ message: "Product updated" });
+});
+
+app.delete("/products/:id", (req, res) => {
+    const { id } = req.params;
+
+    const productIndex = products.findIndex(product => product.id === id);
+
+    products.splice(productIndex, 1);
+
+    return res.json({ message: "Product deleted" });
 })
 
 app.listen(4002, () => console.log("O servidor está rodando na porta 4002"));
